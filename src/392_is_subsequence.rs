08fda9d20658b69c9b4466efda3fc6ec98fbfd1f
SouravWindows <sourav.bhattacharya3@gmail.com>
2025-07-28 17:10:00 +0530

    Initial Commit

---
+++ src/392_is_subsequence.rs
@@ -0,0 +1,12 @@
+struct Solution;
+
+impl Solution {
+    pub fn is_subsequence(s: String, t: String) -> bool {}
+    pub fn greet(s: &str) {
+        println!("Greetings {}", s);
+    }
+}
+
+fn main() {
+    Solution::is_subsequence(s, t);
+}
