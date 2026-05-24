4bd5eb091448594deaccd8faf688288110ebe03f
Sourav-Bhattacharya10 <sourav.bhattacharya3@gmail.com>
2026-05-24 13:28:29 +0530

    20 Valid Parentheses

---
+++ src/20_valid_parentheses.rs
@@ -0,0 +1,75 @@
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+//
+// An input string is valid if:
+//
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+//
+// Example 1:
+//
+// Input: s = "()"
+//
+// Output: true
+//
+// Example 2:
+//
+// Input: s = "()[]{}"
+//
+// Output: true
+//
+// Example 3:
+//
+// Input: s = "(]"
+//
+// Output: false
+//
+// Example 4:
+//
+// Input: s = "([])"
+//
+// Output: true
+//
+// Example 5:
+//
+// Input: s = "([)]"
+//
+// Output: false
+//
+//
+//
+// Constraints:
+//
+// 1 <= s.length <= 104
+// s consists of parentheses only '()[]{}'.
+
+struct Solution;
+
+impl Solution {
+    pub fn is_valid(s: String) -> bool {
+        let mut stack: Vec<char> = Vec::new();
+
+        for ch in s.chars() {
+            if ch == ')' && stack.ends_with(&['(']) {
+                stack.pop();
+            } else if ch == '}' && stack.ends_with(&['{']) {
+                stack.pop();
+            } else if ch == ']' && stack.ends_with(&['[']) {
+                stack.pop();
+            } else {
+                stack.push(ch);
+            }
+        }
+
+        stack.len() == 0
+    }
+}
+
+fn main() {
+    assert_eq!(Solution::is_valid("()".to_string()), true);
+    assert_eq!(Solution::is_valid("()[]{}".to_string()), true);
+    assert_eq!(Solution::is_valid("(]".to_string()), false);
+    assert_eq!(Solution::is_valid("([])".to_string()), true);
+    assert_eq!(Solution::is_valid("([)]".to_string()), false);
+    println!("All tests passed!");
+}
